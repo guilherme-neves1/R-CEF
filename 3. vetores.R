1b3906882e711fe5214e11459c16e713c175c452
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-05-02 18:24:22 -0300

    study R lists

---
+++ 3. vetores.R
@@ -0,0 +1,12 @@
+# Vetores
+# Quando se deseja criar um vetor com mais de um elemento, usa-se a função c() que significa combinar os elementos em um vetor.
+
+# Criar um vetor
+maca <- c('vermelha', 'verde', "amarela")
+print(maca)
+
+# Obtém a classe do vetor
+print(class(maca))
+
+# Listas
+# Uma lista é um objeto R que pode conter muitos tipos diferentes de elementos, como vetores, funções e até outra lista dentro dele.
