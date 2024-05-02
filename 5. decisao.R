c9026de0b1dee375971adaee582ac5ef8e2d2ae7
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-05-02 19:05:50 -0300

    study IF ELSE in R

---
+++ 5. decisao.R
@@ -0,0 +1,16 @@
+# Instrução IF
+x <- 30L
+
+if(is.integer(x)){
+  print("X é um inteiro")
+}
+
+# IF... ELSE
+x <- c("existe", "uma", "verdade")
+
+if("foo" %in% x) {
+  print ("foo foi encontrado")
+} else {
+  print ("foo não foi encontrado")
+}
+
